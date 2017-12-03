888c8cec829b98f03cd201d1e30e9722e6c7c9aa
crc017 <crc017@yahoo.com>
2017-12-03 16:36:58 -0500

    origin master

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+
+var friends = [
+  {
+    name: "Bobby",
+    photo: "https://i.ytimg.com/vi/1u4oXXL1a5A/hqdefault.jpg",
+    scores: [
+      "2",
+      "1",
+      "1",
+      "2",
+      "5"
+    ]
+  },
+  {
+    name: "Joe",
+    photo: "https://i.pinimg.com/736x/7f/ec/4a/7fec4a98da4526dadf920ab7bf92949d--joe-dirt.jpg",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "1"
+    ]
+  },
+  {
+    name: "Jack",
+    photo: "https://s3-media3.fl.yelpcdn.com/bphoto/JL5RtORjKeOIgGtl72swNQ/348s.jpg",
+    scores: [
+      "3",
+      "3",
+      "1",
+      "2",
+      "2",
+    ]
+  }
+];
+
+module.exports = friends;
